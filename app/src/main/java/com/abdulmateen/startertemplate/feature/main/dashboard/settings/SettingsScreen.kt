c300950551faa4895c282f48dab8b5bb1c9493e8
Abdulmateen <abdulmateen6.ch@gmail.com>
2025-11-09 07:59:43 +0500

    Re-structured Architect

--- app/src/main/java/com/abdulmateen/startertemplate/feature/main/dashboard/settings/SettingsScreen.kt
+++ app/src/main/java/com/abdulmateen/startertemplate/feature/main/dashboard/settings/SettingsScreen.kt
@@ -1,11 +1,52 @@
 package com.abdulmateen.startertemplate.feature.main.dashboard.settings
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.WindowInsetsSides
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBars
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ListItem
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.abdulmateen.startertemplate.R
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SettingsScreen(
-    onLogout: () -> Unit
+    onLogout: () -> Unit = {}
 ) {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text(text = stringResource(R.string.settings)) },
+                windowInsets = WindowInsets.statusBars
+            )
+        }
+    ) { 
+        innerPadding ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding)
+        ) {
+            ListItem(
+                headlineContent = { Text(stringResource(R.string.logout)) },
+                modifier = Modifier.clickable { onLogout() }
+            )
+        }
+    }
+}
 
+@Preview
+@Composable
+private fun SettingsScreenPreview() {
+    SettingsScreen()
 }
